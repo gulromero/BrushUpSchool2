a30c32c73b5b2f6a640bff7b47f670cb97665e18
akaics <gulsumerd38@gmail.com>
2024-02-05 22:15:21 +0100

    Add project files.

---
+++ Brush2/Teacher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brush2
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{Id} + {Name} + {Salary}";
+        }
+
+        public void ValidateName()
+        {
+            if (Name == null)
+                throw new ArgumentNullException("Name is null");
+
+            if (Name.Length < 1)
+                throw new ArgumentException("Name must be at least 1 character");
+        }
+
+        public void ValidateSalary()
+        {
+            if (Salary < 0)
+                throw new ArgumentOutOfRangeException("Salary must be non-negative.");
+        }
+
+        public void Validate()
+        {
+            ValidateName();
+            ValidateSalary();
+        }
+    }
+}
---
+++ Brush2Tests/TeacherTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Brush2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brush2.Tests
+{
+    [TestClass()]
+    public class TeacherTests
+    {
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            // Arrange
+            var teacher = new Teacher { Id = 1, Name = "Gulsum Erd", Salary = 50000 };
+
+            // Act
+            string result = teacher.ToString();
+
+            // Assert
+            Assert.AreEqual("1 + Gulsum Erd + 50000", result);
+        }
+
+        [TestMethod()]
+        public void ValidateNameTest()
+        {
+            // Arrange
+            var teacher = new Teacher { Name = "G" };
+
+            // Act and assert
+
+            teacher.ValidateName();
+            
+           
+        }
+
+        [TestMethod()]
+        public void ValidateSalaryTest()
+        {
+            // Arrange
+            var teacher = new Teacher { Salary = -1000 }; // Using a negative salary
+
+            // Act & Assert
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => teacher.ValidateSalary());
+        }
+
+        [TestMethod()]
+        public void ValidateTest()
+        {
+          
+        }
+    }
+}
